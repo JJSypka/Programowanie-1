576bfa833591d650b55fe64a840fd300e2d126d3
JJSypka <jsypka1784@gmail.com>
2022-10-11 19:07:38 +0200

    zadanie dom

---
+++ 02-TypesAndVariables/zad 18 .py
@@ -0,0 +1,6 @@
+a = int (input("Podaj dlugosc pierwszego boku:"))
+b = int (input("Podaj dlugosc drugiego boku:"))
+c = int (input ("Podaj dlugosc trzeciego boku:"))
+
+pole = (a+b+c)/2
+print (pole)
